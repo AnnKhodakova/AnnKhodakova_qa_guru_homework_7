5c2c04ce4b3810f9d95837494e1c923cba02742d
Ann Khodakova <annkhodakova88@gmail.com>
2023-03-21 18:00:22 +0300

    fix

--- tests_for_files.py
+++ tests_for_files.py
@@ -13,22 +13,33 @@ CSV_PATH = f'{DIRECTORY}/username.csv'
 
 @pytest.fixture()
 def create_zip():
-    file_dir = os.listdir(DIRECTORY)
-    with zipfile.ZipFile(ARCHIVE_PATH, mode='w', compression=zipfile.ZIP_DEFLATED) as zf:
-        for file in file_dir:
-            add_file = os.path.join(DIRECTORY, file)
-            zf.write(add_file)
+    try:
+        os.remove(ARCHIVE_PATH)
+    except FileNotFoundError:
+        pass
+    finally:
+        file_dir = os.listdir(DIRECTORY)
+        with zipfile.ZipFile(ARCHIVE_PATH, mode='w', compression=zipfile.ZIP_DEFLATED) as zf:
+            for file in file_dir:
+                add_file = os.path.join(DIRECTORY, file)
+                zf.write(add_file)
 
 
-def xlsx_size_meter(file):
+def define_xlsx_properties(file):
     workbook = load_workbook(file)
     sheet = workbook.active
-    return sheet.max_row, sheet.max_column
+    row_count = sheet.max_row
+    column_count = sheet.max_column
+    header = [sheet.cell(row=3, column=column_index).value for column_index in range(1, column_count)]
+    return row_count, column_count, header
 
 
-def pdf_pages_counter(file):
+def define_pdf_properties(file):
     reader = PdfReader(file)
-    return len(reader.pages)
+    page_count = len(reader.pages)
+    text = reader.pages[0].extract_text()
+    header = text.split('\n')
+    return page_count, header
 
 
 def test_csv(create_zip):
@@ -38,22 +49,22 @@ def test_csv(create_zip):
         with zf.open(CSV_PATH) as archive_csv_file:
             archive_csv_row = len(list(archive_csv_file))
     assert archive_csv_row == csv_row
-    os.remove(ARCHIVE_PATH)
 
 
 def test_pdf(create_zip):
-    pdf_pages = pdf_pages_counter(PDF_PATH)
+    pdf_pages, pdf_header = define_pdf_properties(PDF_PATH)
     with zipfile.ZipFile(ARCHIVE_PATH, 'r') as zf:
         with zf.open(PDF_PATH) as archive_pdf_file:
-            archive_pdf_pages = pdf_pages_counter(archive_pdf_file)
+            archive_pdf_pages, archive_pdf_header = define_pdf_properties(archive_pdf_file)
     assert archive_pdf_pages == pdf_pages
-    os.remove(ARCHIVE_PATH)
+    assert archive_pdf_header == pdf_header
 
 
 def test_xlsx(create_zip):
-    xlsx_row, xlsx_column = xlsx_size_meter(XLSX_PATH)
+    xlsx_row, xlsx_column, xlsx_header = define_xlsx_properties(XLSX_PATH)
     with zipfile.ZipFile(ARCHIVE_PATH, 'r') as zf:
         with zf.open(XLSX_PATH) as archive_xlsx_file:
-            archive_xlsx_row, archive_xlsx_column = xlsx_size_meter(archive_xlsx_file)
-    assert archive_xlsx_row == xlsx_row and archive_xlsx_column == xlsx_column
-    os.remove(ARCHIVE_PATH)
+            archive_xlsx_row, archive_xlsx_column, archive_xlsx_header = define_xlsx_properties(archive_xlsx_file)
+    assert archive_xlsx_row == xlsx_row
+    assert archive_xlsx_column == xlsx_column
+    assert archive_xlsx_header == xlsx_header
